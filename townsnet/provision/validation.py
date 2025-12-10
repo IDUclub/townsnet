b10b585d2ad88d182d77edfba68bedaf10cc0e92
Roman <roma.bashirov.1@yandex.ru>
2025-12-10 15:38:19 +0300

    upd validation columns

--- townsnet/provision/validation.py
+++ townsnet/provision/validation.py
@@ -4,7 +4,7 @@ from pandera.typing import Series
 
 class GeoDfSchema(pa.DataFrameModel):
     name: Series[str]
-    is_city: Series[bool]
+    is_anchor: Series[bool]
     population: Series[float]
     geometry: Series
 
@@ -15,7 +15,7 @@ class GeoDfSchema(pa.DataFrameModel):
 # Функция валидации
     @classmethod
     def validate(cls, gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-        required = ["name", "is_city", "population", "geometry"]
+        required = ["name", "is_anchor", "population", "geometry"]
         missing = [col for col in required if col not in gdf.columns]
         if missing:
             raise ValueError(f"Missing required columns: {missing}")
